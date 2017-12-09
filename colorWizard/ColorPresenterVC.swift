fa57993a95056598c214a22523fa28f9d965254e
Adrian Thomas <adrian.l.thomas@outlook.com>
2017-12-09 16:02:26 -0600

    creates an IBOutlet for the UILabel

---
+++ colorWizard/ColorPresenterVC.swift
@@ -0,0 +1,22 @@
+//
+// ColorPresenterVC.swift
+//  colorWizard
+//
+//  Created by Adrian Thomas on 12/9/17.
+//  Copyright © 2017 Adrian Thomas. All rights reserved.
+//
+
+import UIKit
+
+class ColorPresenterVC: UIViewController {
+
+  @IBOutlet weak var colorNameLbl: UILabel!
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+
+}
+
